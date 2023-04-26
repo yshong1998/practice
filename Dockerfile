d1f188e9372ec23f9f5dc42ed1c23c9012f1614d
yshong1998 <yshong1998@gmail.com>
2023-04-27 01:35:44 +0900

    이전 설정 초기화

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM openjdk:11-jdk-alpine
-ENV TZ="Asia/Seoul"
-ARG JAR_FILE=build/libs/practice-0.0.1-SNAPSHOT.jar
-COPY ${JAR_FILE} app.jar
-
-HEALTHCHECK --interval=5s --timeout=5s --start-period=15s --retries=10 CMD wget http://localhost:8080/actuator/health --quiet --output-document - >/dev/null 2>&1
-
-ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/example/practice/controller/TestController.java
+++ src/main/java/com/example/practice/controller/TestController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,14 +15,7 @@ public class TestController {
 
     @GetMapping("test/port")
     public String checkServerPort(){
-        List<String> profile = Arrays.asList(env.getActiveProfiles());
-        List<String> realProfiles = Arrays.asList("real1", "real2");
-        String defaultProfile = profile.isEmpty() ? "default" : profile.get(0);
-
-        return profile.stream()
-                .filter(realProfiles::contains)
-                .findAny()
-                .orElse(defaultProfile);
+        return Arrays.stream(env.getActiveProfiles()).findFirst().orElse("");
     }
 
 }
